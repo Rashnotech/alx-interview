befd8015ce9961adbe3d97fb870117dfbe4de682
Rashnotech <rashnotech@gmail.com>
2024-01-08 04:01:40 +0000

    Fixed: pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -8,13 +8,13 @@ def pascal_triangle(n):
     n: is a positive integer value to generate output
     Return: an empty list if an n is less than 0 or equal 0.
     """
-    pascal = []
     if n <= 0:
         return pascal
-    for i in range(n):
-        tx_s = str(11 ** i)
-        tx_list = []
-        for num in tx_s:
-            tx_list.append(num)
-        pascal.append(tx_list)
+    pascal = [[1]]
+    for i in range(1, n):
+        innerList = [1]
+        for idx in range(len(pascal[i - 1]) - 1):
+            innerList.append(pascal[i - 1][idx] + pascal[i - 1][idx + 1])
+        innerList.append(1)
+        pascal.append(innerList)
     return pascal
