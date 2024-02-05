017fb1d79300c3a3bcb384ec8eea9aabe1d466d1
Rashnotech <rashnotech@gmail.com>
2024-02-05 14:15:12 +0000

    A function that validate UTF 8

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" a module that determines if a given set is valid UTF"""
+
+def validUTF8(data):
+    """Return True if data is a valid UTF-8"""
+    try:
+        bytes(data).decode('utf-8')
+        return True
+    except Exception:
+        return False
