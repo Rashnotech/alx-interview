3670624570e5ccbe3f94d6127a409a677594ca18
Rashnotech <rashnotech@gmail.com>
2024-02-20 09:58:45 +0000

    Fixed: star wars characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,30 +1,32 @@
 #!/usr/bin/node
 /**
- * a script that prints all characters of star wars movie
+ * a script that prints all characters of a star wars movie
  */
-const args = require('process').argv
-const request = require('request')
+const args = require('process').argv;
+const request = require('request');
 
-const id = args[2]
+const id = args[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
-const fetchApi = (id) => {
+const fetchApi = (url) => {
   return new Promise((resolve, reject) => {
     request(url, (error, response, body) => {
+      if (error) console.log(error);
       if (response.statusCode === 200) {
-        const characters = JSON.parse(body).characters;
-	resolve(characters)
+        const characters = JSON.parse(body).name;
+        resolve(characters);
       } else {
         reject(response);
       }
-    })
-  }
-}
+    });
+  });
+};
 
 request(url, (error, response, body) => {
+  if (error) console.log(error);
   const characters = JSON.parse(body).characters;
-  const promises = characters.map(character => fetchApi(character));
-  Promise.all(promises)
-  .then(results = results.forEach(result, console.log(result))
-  .catch(error => console.log(error));
+  const promise = characters.map(character => fetchApi(character));
+  Promise.all(promise)
+    .then(results => results.forEach(result => console.log(result)))
+    .catch(error => console.log(error));
 });
