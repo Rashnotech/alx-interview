72fc84606f182b0cacc5a2b901f01f3789bf050d
Rashnotech <rashnotech@gmail.com>
2024-03-10 01:42:22 +0000

    Island perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""a module that finds the perimeter of the island"""
+
+
+def island_perimeter(grid):
+    """
+    grid: is a list of integers
+    """
+    visit = set()
+
+    def dfs(i, j):
+        """Depth first search"""
+        if i >= len(grid) or j >= len(grid[0]) or i < 0 or j < 0\
+                or grid[i][j] == 0:
+            return 1
+        if (i, j) in visit:
+            return 0
+        visit.add((i, j))
+        perim = dfs(i, j + 1)
+        perim += dfs(i + 1, j)
+        perim += dfs(i, j - 1)
+        perim += dfs(i - 1, j)
+        return perim
+
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j]:
+                return dfs(i, j)
